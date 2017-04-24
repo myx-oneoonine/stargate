af330446876921409d94be8fa8182bbd3f914e67
Phanuphat Cholatda <remix.tk108@gmail.com>
2017-04-24 11:31:30 +0700

    Add network script สำหรับ unix ปรับ script disable ให้ สามารถหา ip ที่ต้องการ kill ได้ชัดขึ้น

--- client_disable_script.sh
+++ client_disable_script.sh
@@ -3,9 +3,9 @@
 ##you must change mod this script first
 ##chmod a+x client_diable_script.sh
 
-pid=$(ps -ef | grep $(echo $(cat ip | head -1 | sed 's/:.*//')) | awk '{print $2}')
+pid=$(ps -ef | grep ssh.*$(echo $(cat ip | head -1 | sed 's/:.*//')) | awk '{print $2}')
 
-for p in $pid do
+for p in $pid ; do
   echo "killing $p..."
   sudo kill -9 $p
   echo "killed $p"
