efce8929550c743f854c0268f9774d2db6363a87
Phanuphat Cholatda <remix.tk108@gmail.com>
2017-10-11 14:27:31 +0000

    Update add route table.bat 10.138.x.x, 10.139.x.x

--- route table/add route table.bat
+++ route table/add route table.bat
@@ -7,6 +7,7 @@ route add 10.15.0.0 mask 255.255.0.0 10.239.220.1 metric 1 -p
 route add 10.197.0.0 mask 255.255.0.0 10.239.220.1 metric 1 -p
 route add 10.239.0.0 mask 255.255.0.0 10.239.220.1 metric 1 -p
 route add 172.16.0.0 mask 255.255.0.0 10.239.220.1 metric 1 -p
-
+route add 10.138.0.0 mask 255.255.0.0 10.239.220.1 metric 1 -p
+route add 10.139.0.0 mask 255.255.0.0 10.239.220.1 metric 1 -p
 
 pause
